ed446bcf099fa6daf0382984eb97f09efd003034
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-08-24 11:59:42 -0400

    new repo works

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shroom Burger', FALSE);
